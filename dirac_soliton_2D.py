1f9d0016ee799fd0800ba15cfcaeec5526a2f7f6
Đoàn Tùng Anh <doantunganh@gmail.com>
2021-07-05 20:30:17 +0700

    Update a_m
    Update a_m, but still trouble with indexing difference between Matlab and python

--- dirac_soliton_2D.py
+++ dirac_soliton_2D.py
@@ -20,14 +20,23 @@ def dirac_soliton_2D():
     
     a_n = np.zeros(len(nvec2), dtype = np.complex128)
     for x in range(1, round((wgc.COL_NUM - 1) / 2)):
-        a_n[2 * x - 1] = 1j** (2 * x) * coeff_1 / math.cosh((nvec2[2 * x - 1] - ncenter) / wgc.BEAMWIDTH)
-        a_n[2 * x] = 1j** (2 * x) * coeff_2 / math.cosh((nvec2[2 * x] - ncenter) / wgc.BEAMWIDTH) * math.tanh((nvec2[2 * x] - ncenter) / wgc.BEAMWIDTH)
+        a_n[2 * x - 1] = 1j ** (2 * x) * coeff_1 / math.cosh((nvec2[2 * x - 1] - ncenter) / wgc.BEAMWIDTH)
+        a_n[2 * x] = 1j ** (2 * x) * coeff_2 / math.cosh((nvec2[2 * x] - ncenter) / wgc.BEAMWIDTH) * math.tanh((nvec2[2 * x] - ncenter) / wgc.BEAMWIDTH)
+    
+    a_n[wgc.COL_NUM] = 1j ** (wgc.COL_NUM + 1) * coeff_1 / math.cosh((nvec2[wgc.COL_NUM] - ncenter) / wgc.BEAMWIDTH);
+    
+    a_m = np.zeros(len(mvec2), dtype = np.complex128)
+    for x in range(1, round((wgc.ROW_NUM - 1) / 2)):
+        a_m[2 * x - 1] = 1j ** (2 * x) * coeff_1 / math.cosh((mvec2[2 * x - 1] - ncenter) / wgc.BEAMWIDTH)
+        a_m[2 * x] = 1j ** (2 * x) * coeff_2 / math.cosh((mvec2[2 * x] - ncenter) / wgc.BEAMWIDTH) * math.tanh((mvec2[2 * x] - ncenter) / wgc.BEAMWIDTH)
+    
+    a_m[wgc.ROW_NUM] = 1j ** (wgc.ROW_NUM + 1) * coeff_1 / math.cosh((mvec2[wgc.ROW_NUM] - ncenter) / wgc.BEAMWIDTH);
     
     return col_vec, row_vec, nvec2, mvec2, ncenter, mcenter, center, coeff_1, coeff_2, a_n                               
 
 col_vec, row_vec, nvec2, mvec2, ncenter, mcenter, center, coeff_1, coeff_2, a_n = dirac_soliton_2D()
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x = nvec2, y = a_n.real))
+fig.add_trace(go.Scatter(x = nvec2, y = abs(a_n.real)))
 fig.write_html("s.html",auto_open=True)
 

51b3e4fea5825dbf307894aa8e466f204d01e92e
Đoàn Tùng Anh <doantunganh@gmail.com>
2021-07-05 16:22:49 +0700

    Calculate Dirac array and plot Dirac Soliton
    Calculate Dirac a_n and plot Dirac Soliton

--- dirac_soliton_2D.py
+++ dirac_soliton_2D.py
@@ -1,17 +1,33 @@
 import wgconstants as wgc
 import math
 import numpy as np
+import plotly.graph_objects as go
 
 def dirac_soliton_2D():
-    step_num = round(wgc.Z_LIMIT/wgc.Z_STEP)            # number of steps
-    index_taken = math.floor(step_num/wgc.FRAME_NUM)
-    col_vec = np.linspace((-wgc.COL_NUM-1)/2,(wgc.COL_NUM-1)/2,wgc.COL_NUM)
-    row_vec = np.linspace((-wgc.ROW_NUM-1)/2,(wgc.ROW_NUM-1)/2,wgc.ROW_NUM)
+#    step_num = round(wgc.Z_LIMIT/wgc.Z_STEP)                                                # number of steps
+#    index_taken = math.floor(step_num/wgc.FRAME_NUM)
+    col_vec = np.linspace((-wgc.COL_NUM - 1) / 2, (wgc.COL_NUM - 1) / 2, wgc.COL_NUM)
+    row_vec = np.linspace((-wgc.ROW_NUM - 1)/ 2, (wgc.ROW_NUM -1 ) / 2, wgc.ROW_NUM)
     nvec2 = np.arange(start = 1, stop = wgc.COL_NUM, step = 1)
     mvec2 = np.arange(start = 1, stop = wgc.ROW_NUM, step = 1)
-    ncenter = (wgc.COL_NUM+1)/2;
-    mcenter = (wgc.ROW_NUM+1)/2;
+    ncenter = (wgc.COL_NUM + 1) / 2;
+    mcenter = (wgc.ROW_NUM + 1) / 2;
     center  = mcenter + ncenter; 
-    return col_vec, row_vec, nvec2, mvec2, ncenter, mcenter, center                               
+    
+    # Calculate Dirac Soliton 
+    coeff_1 = 2 * wgc.COUPLING_COEF_1 / wgc.BEAMWIDTH / math.sqrt(abs(wgc.SIGMA * wgc.GAMMA))
+    coeff_2 = 2 * math.pow(wgc.COUPLING_COEF_1, 2) / math.pow(wgc.BEAMWIDTH, 2) / wgc.SIGMA / math.sqrt(abs(wgc.SIGMA * wgc.GAMMA))
+    
+    a_n = np.zeros(len(nvec2), dtype = np.complex128)
+    for x in range(1, round((wgc.COL_NUM - 1) / 2)):
+        a_n[2 * x - 1] = 1j** (2 * x) * coeff_1 / math.cosh((nvec2[2 * x - 1] - ncenter) / wgc.BEAMWIDTH)
+        a_n[2 * x] = 1j** (2 * x) * coeff_2 / math.cosh((nvec2[2 * x] - ncenter) / wgc.BEAMWIDTH) * math.tanh((nvec2[2 * x] - ncenter) / wgc.BEAMWIDTH)
+    
+    return col_vec, row_vec, nvec2, mvec2, ncenter, mcenter, center, coeff_1, coeff_2, a_n                               
 
-col_vec, row_vec, nvec2, mvec2, ncenter, mcenter, center = dirac_soliton_2D()
+col_vec, row_vec, nvec2, mvec2, ncenter, mcenter, center, coeff_1, coeff_2, a_n = dirac_soliton_2D()
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x = nvec2, y = a_n.real))
+fig.write_html("s.html",auto_open=True)
+

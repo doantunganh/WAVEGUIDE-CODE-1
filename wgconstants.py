25420b7238fb7bd9cc62ec26e00558071cd7432f
Đoàn Tùng Anh <doantunganh@gmail.com>
2021-07-05 09:32:54 +0700

    Create wgconstants.py
    create file to hold constants

---
+++ wgconstants.py
@@ -0,0 +1,14 @@
+Z_LIMIT = 80            # LIMITATION ON Z AXIS
+Z_STEP = 0.0005         # 
+BEAMWIDTH = 5
+COUPLING_COEF_1 = 1
+COUPLING_COEF_2 = 0.5
+SIGMA = -1.2            # DIRAC MASS
+GAMMA = 1               # NONLINEARITY
+COL_NUM = 121           # COLUMN NUMBER
+ROW_NUM = 121           # ROW NUMBER
+
+FRAME_NUM = 80
+
+
+

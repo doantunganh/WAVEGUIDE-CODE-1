dd646d9de733297523c448349f303bd57a5a0aa5
Đoàn Tùng Anh <doantunganh@gmail.com>
2021-07-05 21:09:26 +0700

    quick fix but need to revise index
    Index start from 0

--- dirac_soliton_2D.py
+++ dirac_soliton_2D.py
@@ -8,8 +8,8 @@ def dirac_soliton_2D():
 #    index_taken = math.floor(step_num/wgc.FRAME_NUM)
     col_vec = np.linspace((-wgc.COL_NUM - 1) / 2, (wgc.COL_NUM - 1) / 2, wgc.COL_NUM)
     row_vec = np.linspace((-wgc.ROW_NUM - 1)/ 2, (wgc.ROW_NUM -1 ) / 2, wgc.ROW_NUM)
-    nvec2 = np.arange(start = 1, stop = wgc.COL_NUM, step = 1)
-    mvec2 = np.arange(start = 1, stop = wgc.ROW_NUM, step = 1)
+    nvec2 = np.arange(start = 0, stop = wgc.COL_NUM, step = 1)
+    mvec2 = np.arange(start = 0, stop = wgc.ROW_NUM, step = 1)
     ncenter = (wgc.COL_NUM + 1) / 2;
     mcenter = (wgc.ROW_NUM + 1) / 2;
     center  = mcenter + ncenter; 
@@ -23,14 +23,14 @@ def dirac_soliton_2D():
         a_n[2 * x - 1] = 1j ** (2 * x) * coeff_1 / math.cosh((nvec2[2 * x - 1] - ncenter) / wgc.BEAMWIDTH)
         a_n[2 * x] = 1j ** (2 * x) * coeff_2 / math.cosh((nvec2[2 * x] - ncenter) / wgc.BEAMWIDTH) * math.tanh((nvec2[2 * x] - ncenter) / wgc.BEAMWIDTH)
     
-    a_n[wgc.COL_NUM] = 1j ** (wgc.COL_NUM + 1) * coeff_1 / math.cosh((nvec2[wgc.COL_NUM] - ncenter) / wgc.BEAMWIDTH);
+    a_n[wgc.COL_NUM-1] = 1j ** (wgc.COL_NUM + 1) * coeff_1 / math.cosh((nvec2[wgc.COL_NUM-1] - ncenter) / wgc.BEAMWIDTH);
     
     a_m = np.zeros(len(mvec2), dtype = np.complex128)
     for x in range(1, round((wgc.ROW_NUM - 1) / 2)):
         a_m[2 * x - 1] = 1j ** (2 * x) * coeff_1 / math.cosh((mvec2[2 * x - 1] - ncenter) / wgc.BEAMWIDTH)
         a_m[2 * x] = 1j ** (2 * x) * coeff_2 / math.cosh((mvec2[2 * x] - ncenter) / wgc.BEAMWIDTH) * math.tanh((mvec2[2 * x] - ncenter) / wgc.BEAMWIDTH)
     
-    a_m[wgc.ROW_NUM] = 1j ** (wgc.ROW_NUM + 1) * coeff_1 / math.cosh((mvec2[wgc.ROW_NUM] - ncenter) / wgc.BEAMWIDTH);
+    a_m[wgc.ROW_NUM-1] = 1j ** (wgc.ROW_NUM + 1) * coeff_1 / math.cosh((mvec2[wgc.ROW_NUM-1] - ncenter) / wgc.BEAMWIDTH);
     
     return col_vec, row_vec, nvec2, mvec2, ncenter, mcenter, center, coeff_1, coeff_2, a_n                               
 

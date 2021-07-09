095db3e4ad0fa559dda166261aa16bb0c5b3c66e
Đoàn Tùng Anh <doantunganh@gmail.com>
2021-07-09 16:30:53 +0700

    Synchronized python and matlab
    The calculated matrix synchronized

--- dirac_soliton_2D.py
+++ dirac_soliton_2D.py
@@ -6,11 +6,11 @@ import plotly.graph_objects as go
 def dirac_soliton_2D():
 #    step_num = round(wgc.Z_LIMIT/wgc.Z_STEP)                                                # number of steps
 #    index_taken = math.floor(step_num/wgc.FRAME_NUM)
-    col_vec = np.linspace((-wgc.COL_NUM - 1) / 2, (wgc.COL_NUM - 1) / 2, wgc.COL_NUM)
-    row_vec = np.linspace((-wgc.ROW_NUM - 1)/ 2, (wgc.ROW_NUM -1 ) / 2, wgc.ROW_NUM)
-    nvec2 = np.arange(start = 0, stop = wgc.COL_NUM, step = 1)
-    mvec2 = np.arange(start = 0, stop = wgc.ROW_NUM, step = 1)
-    ncenter = (wgc.COL_NUM + 1) / 2;
+#    col_vec = np.linspace(start = -(wgc.COL_NUM - 1) / 2, stop = (wgc.COL_NUM - 1) / 2, num = wgc.COL_NUM)
+#    row_vec = np.linspace(start = -(wgc.ROW_NUM - 1) / 2, stop = (wgc.ROW_NUM - 1) / 2, num = wgc.ROW_NUM)
+    nvec2 = np.arange(start = 0, stop = wgc.COL_NUM + 1, step = 1)
+    mvec2 = np.arange(start = 0, stop = wgc.ROW_NUM + 1, step = 1)
+    ncenter = (wgc.COL_NUM + 1) / 2;     # COL_NUM = 121
     mcenter = (wgc.ROW_NUM + 1) / 2;
     center  = mcenter + ncenter; 
     
@@ -18,23 +18,26 @@ def dirac_soliton_2D():
     coeff_1 = 2 * wgc.COUPLING_COEF_1 / wgc.BEAMWIDTH / math.sqrt(abs(wgc.SIGMA * wgc.GAMMA))
     coeff_2 = 2 * math.pow(wgc.COUPLING_COEF_1, 2) / math.pow(wgc.BEAMWIDTH, 2) / wgc.SIGMA / math.sqrt(abs(wgc.SIGMA * wgc.GAMMA))
     
-    a_n = np.zeros(len(nvec2), dtype = np.complex128)
-    for x in range(1, round((wgc.COL_NUM - 1) / 2)):
+    a_n = np.zeros(len(nvec2), dtype = np.complex128)     # 122 allocations, leaving first allocation zero
+    
+    for x in range(1, round((wgc.COL_NUM - 1) / 2 + 1)):  # 1-60
+        # Odd waveguide position (counted from 0)
         a_n[2 * x - 1] = 1j ** (2 * x) * coeff_1 / math.cosh((nvec2[2 * x - 1] - ncenter) / wgc.BEAMWIDTH)
+        # Even waveguide position (couted from 1)
         a_n[2 * x] = 1j ** (2 * x) * coeff_2 / math.cosh((nvec2[2 * x] - ncenter) / wgc.BEAMWIDTH) * math.tanh((nvec2[2 * x] - ncenter) / wgc.BEAMWIDTH)
     
-    a_n[wgc.COL_NUM-1] = 1j ** (wgc.COL_NUM + 1) * coeff_1 / math.cosh((nvec2[wgc.COL_NUM-1] - ncenter) / wgc.BEAMWIDTH);
+    a_n[wgc.COL_NUM] = 1j ** (wgc.COL_NUM + 1) * coeff_1 / math.cosh((nvec2[wgc.COL_NUM] - ncenter) / wgc.BEAMWIDTH);
     
     a_m = np.zeros(len(mvec2), dtype = np.complex128)
-    for x in range(1, round((wgc.ROW_NUM - 1) / 2)):
+    for x in range(1, round((wgc.ROW_NUM - 1) / 2 + 1)):
         a_m[2 * x - 1] = 1j ** (2 * x) * coeff_1 / math.cosh((mvec2[2 * x - 1] - ncenter) / wgc.BEAMWIDTH)
         a_m[2 * x] = 1j ** (2 * x) * coeff_2 / math.cosh((mvec2[2 * x] - ncenter) / wgc.BEAMWIDTH) * math.tanh((mvec2[2 * x] - ncenter) / wgc.BEAMWIDTH)
     
-    a_m[wgc.ROW_NUM-1] = 1j ** (wgc.ROW_NUM + 1) * coeff_1 / math.cosh((mvec2[wgc.ROW_NUM-1] - ncenter) / wgc.BEAMWIDTH);
+    a_m[wgc.ROW_NUM] = 1j ** (wgc.ROW_NUM + 1) * coeff_1 / math.cosh((mvec2[wgc.ROW_NUM] - ncenter) / wgc.BEAMWIDTH);
     
-    return col_vec, row_vec, nvec2, mvec2, ncenter, mcenter, center, coeff_1, coeff_2, a_n                               
+    return nvec2, mvec2, ncenter, mcenter, center, coeff_1, coeff_2, a_n, a_m                               
 
-col_vec, row_vec, nvec2, mvec2, ncenter, mcenter, center, coeff_1, coeff_2, a_n = dirac_soliton_2D()
+nvec2, mvec2, ncenter, mcenter, center, coeff_1, coeff_2, a_n, a_m = dirac_soliton_2D()
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x = nvec2, y = abs(a_n.real)))
